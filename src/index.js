78ae12b252e5aa5413ce9e6409c177b98c360aa9
VitaliyKorzhevyi <111433531+VitaliyKorzhevyi@users.noreply.github.com>
2024-02-02 10:29:15 +0200

    HW6

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
     <ToastContainer
         position="top-center"
         autoClose={4000}
